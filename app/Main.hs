15713fc17dc57f832bb9aae395808344c800e378
aj <aj@debian>
2017-02-24 13:47:06 -0500

    Movement appears to work!

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,23 @@
 module Main where
 
+import           Control.Lens
 import           Data.Either
+import           Data.List
+import qualified Data.Map.Strict as M
 import           Tak.Parser.PTN
-import           Tak.Printer.PTN
 import           Tak.Types
 import           Text.Trifecta
 
 main :: IO ()
-main =  do
-  mvs <- getMoves "./ptn/game1.ptn"
-  mapM_ putStrLn (printMoves mvs)
+main =  print 1
+
+getGame :: IO [GameState]
+getGame =  do
+  mvs <- concatMap rights <$> getMoves "./ptn/game1.ptn"
+  return $ scanl' makeMove (initialGameState 6) mvs
+
+printBoard :: GameState -> IO ()
+printBoard gs = mapM_ print $ M.toList (gs^.gsBoard)
 
 getMoves :: String -> IO [[Either GameOverState Move]]
 getMoves f = do
--- src/Tak/Types.hs
+++ src/Tak/Types.hs
@@ -3,16 +3,26 @@
 module Tak.Types where
 
 import           Control.Lens
-import           Data.DList      (DList)
-import qualified Data.DList      as DL
-import           Data.List
-import           Data.Map.Strict (Map)
-import qualified Data.Map.Strict as M
+import           Data.DList       (DList)
+import qualified Data.DList       as DL
+import           Data.Foldable
+import           Data.Graph.AStar
+import qualified Data.HashSet     as H
+import           Data.Map.Strict  (Map)
+import qualified Data.Map.Strict  as M
+import           Data.Maybe
+import           Safe
 
-type Rank = Int
 type File = Char
+type Rank = Int
 type Coord = (File, Rank)
+
+isAdjacent :: Coord -> Coord -> Bool
+isAdjacent c1 c2 = any ((==) c2 . flip goDirection c1) [L,R,U,D]
+
 type BoardSize = Int
+type BoardState = Map Coord [(Player,PieceType)]
+type PieceSupply = Map Player (Int, Int)
 
 data Player = Player1 | Player2
   deriving (Eq, Show, Ord)
@@ -25,16 +35,17 @@ data Direction = L | R | U | D
   deriving (Eq, Show)
 
 goDirection :: Direction -> Coord -> Coord
-goDirection U = _2 %~ succ
-goDirection D = _2 %~ pred
-goDirection R = _1 %~ succ
-goDirection L = _1 %~ pred
+goDirection U = over _2 succ
+goDirection D = over _2 pred
+goDirection R = over _1 succ
+goDirection L = over _1 pred
 
 data PieceType = Flat | Standing | Cap
   deriving (Eq, Show)
 
 data Move = Place PieceType Coord
           | Move Int Coord Direction [Int]
+          | Resign
   deriving (Eq, Show)
 
 data GameOverState = RoadWin Player
@@ -44,14 +55,19 @@ data GameOverState = RoadWin Player
   deriving (Eq,Show)
 
 data GameState = GameState
-  { _gsBoard         :: Map Coord [(Player,PieceType)]
+  { _gsBoard         :: BoardState
   , _gsMoves         :: DList Move
   , _gsCurrPlayer    :: Player
-  , _gsSupplyPieces  :: Map Player (Int, Int)
+  , _gsSupplyPieces  :: PieceSupply
   , _gsGameOverState :: Maybe GameOverState
+  , _gsBoardSize     :: BoardSize
   }
   deriving (Eq,Show)
 makeLenses ''GameState
+
+nonStanding :: Player -> BoardState -> BoardState
+nonStanding p = M.filter (maybe False isPlayerStanding . headMay)
+  where isPlayerStanding (pl,pT) = pT /= Standing && pl == p
 
 initialGameState :: BoardSize -> GameState
 initialGameState s = GameState
@@ -61,6 +77,7 @@ initialGameState s = GameState
   , _gsCurrPlayer = Player1
   , _gsSupplyPieces = M.insert Player2 (pieces s) $ M.insert Player1 (pieces s) M.empty
   , _gsGameOverState = Nothing
+  , _gsBoardSize = s
   }
   where
     pieces 3 = (10,0)
@@ -71,21 +88,73 @@ initialGameState s = GameState
     pieces _ = error "Can't play a game of this size"
 
 makeMove :: GameState -> Move -> GameState
-makeMove gs m = GameState
-  { _gsMoves = DL.snoc (gs^.gsMoves) m
-  , _gsBoard = updateBoard gs m
-  , _gsCurrPlayer = nextPlayer $ gs^.gsCurrPlayer
-  , _gsSupplyPieces = case m of
-    (Place Cap _) -> updateSupply _2
-    (Place _ _)   -> updateSupply _1
-    _             -> gs^.gsSupplyPieces
+makeMove gs m  = GameState
+  { _gsBoardSize = gs^.gsBoardSize
+  , _gsMoves = updatedMoves
+  , _gsBoard = updatedBoard
+  , _gsCurrPlayer = updatedPlayer
+  , _gsSupplyPieces = updatedSupply
+  , _gsGameOverState = updatedGameOver
   }
   where
-    updateSupply l = M.update (Just . over l pred) (gs^.gsCurrPlayer) (gs^.gsSupplyPieces)
+    playersMove = if length (DL.toList (gs^.gsMoves)) < 2
+                  then nextPlayer (gs^.gsCurrPlayer)
+                  else gs^.gsCurrPlayer
+    updatedMoves = DL.snoc (gs^.gsMoves) m
+    updatedPlayer = nextPlayer $ gs^.gsCurrPlayer
+    updatedBoard = updateBoard playersMove (gs^.gsBoard) m
+    updatedSupply = case m of
+      (Place Cap _) -> updateSupply _2
+      (Place _ _)   -> updateSupply _1
+      _             -> gs^.gsSupplyPieces
+    updateSupply l = M.update (Just . over l pred) playersMove (gs^.gsSupplyPieces)
+    updatedGameOver = case m of
+      Resign  -> Just (ResignWin updatedPlayer)
+      _ -> case roadWon of
+        Nothing -> flatWin updatedSupply updatedBoard
+        _ -> roadWon
+    roadWon = roadWin (gs^.gsCurrPlayer) (gs^.gsBoardSize) updatedBoard
 
-updateBoard :: GameState -> Move -> Map Coord [(Player,PieceType)]
-updateBoard gs (Place pT c) = M.insert c [(gs^.gsCurrPlayer, pT)] $ gs^.gsBoard
-updateBoard gs (Move i c d xs) = go (gs^.gsBoard) i c xs
+road :: Player -> BoardSize -> BoardState -> Maybe [Coord]
+road p s b = headMay $ mapMaybe shortestPath edgePairs
+  where
+    shortestPath (c1,c2) = (:) c1 <$> aStar adjacents (const . const 1) (const 1) (== c2) c1
+    coords = M.keys $ nonStanding p b
+    adjacents c = H.fromList $ filter (isAdjacent c) coords
+    edgePairs =  [(x,y) | x <- firstRank, y <- lastRank]
+              ++ [(x,y) | x <- firstFile, y <- lastFile]
+    firstRank = filter ((==) 1 . snd) coords
+    lastRank = filter ((==) s . snd) coords
+    firstFile = filter ((==) 'a' . fst) coords
+    lastFile = filter ((==) lastFileChr . fst) coords
+    lastFileChr = last $ take s ['a'..]
+
+roadWin :: Player -> BoardSize -> BoardState -> Maybe GameOverState
+roadWin p s bs
+  | doubleRoad    = Just (RoadWin p)
+  | isJust p1Road = Just (RoadWin Player1)
+  | isJust p2Road = Just (RoadWin Player2)
+  | otherwise     = Nothing
+  where
+    doubleRoad = isJust p1Road && isJust p2Road
+    p1Road = road Player1 s bs
+    p2Road = road Player2 s bs
+
+flatWin :: PieceSupply -> BoardState -> Maybe GameOverState
+flatWin ps bs
+  | noneInSupply = case compare p1Count p2Count of
+                   GT -> Just (FlatWin Player1)
+                   LT -> Just (FlatWin Player2)
+                   EQ -> Just Draw
+  | otherwise = Nothing
+  where
+    p1Count = M.size $ nonStanding Player1 bs
+    p2Count = M.size $ nonStanding  Player2 bs
+    noneInSupply = elem 0 $ fmap (uncurry (+)) ps
+
+updateBoard :: Player -> BoardState -> Move -> BoardState
+updateBoard player bs (Place pT c) = M.insert c [(player, pT)] bs
+updateBoard _ bs (Move i c d xs) = go bs i c xs
   where
     go m _ _ [] = m
     go m i' c' (x':xs') = go newBoard (i' - x') newCoord xs'
@@ -93,14 +162,14 @@ updateBoard gs (Move i c d xs) = go (gs^.gsBoard) i c xs
         newCoord = goDirection d c'
         pcs@(p:_) = take i' $ m M.! c'
         removePcs = M.update (Just . drop i') c' m
-        newBoard = M.update (Just . (++) pcs . smashTopPieceIfNeeded) newCoord removePcs
+        newBoard = M.update (Just . (++) pcs . smashIfNeeded) newCoord removePcs
         smashPossible = i' == 1 && null xs' && x' == 1 && snd p == Cap
-        smashTopPieceIfNeeded
+        smashIfNeeded
           | smashPossible = over (ix 0 . _2) flattenWall
           | otherwise = id
         flattenWall Standing = Flat
         flattenWall s = s
-
+updateBoard _ bs _ = bs
 
 g1 = makeMove (initialGameState 4) (Place Flat ('a',1))
 g2 = makeMove g1 (Place Flat ('a',2))
@@ -113,3 +182,12 @@ g8 = makeMove g7 (Place Standing ('b',1))
 g9 = makeMove g8 (Place Cap ('b',2))
 g10 = makeMove g9 (Place Flat ('c',1))
 g11 = makeMove g10 (Move 1 ('b',2) D [1])
+g12 = makeMove g11 (Place Flat ('d',4))
+
+w1 = makeMove (initialGameState 4) (Place Flat ('a',1))
+w2 = makeMove w1 (Place Flat ('b',1))
+w3 = makeMove w2 (Place Flat ('a',2))
+w4 = makeMove w3 (Place Flat ('b',2))
+w5 = makeMove w4 (Place Flat ('a',3))
+w6 = makeMove w5 (Place Flat ('b',3))
+w7 = makeMove w6 (Place Cap ('a',4))
