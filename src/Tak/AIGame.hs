ad93d9390f9f034b3f60c7240299676e7ca9640e
aj <aj@debian>
2017-03-24 13:27:18 -0400

    Can play an AI!

---
+++ src/Tak/AIGame.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Tak.AIGame where
+
+import           Control.Lens
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.State
+import           Data.Maybe
+import           Safe                         (readMay)
+import           System.Random
+import           Tak.Parser.PTN
+import           Tak.Printer.PTN
+import           Tak.Types
+import           Text.PrettyPrint.ANSI.Leijen (putDoc)
+import           Text.Trifecta
+
+type AI = GameState -> IO Move
+data AIGameState = AIGameState
+    { _aiGameState   :: GameState
+    , _aiHumanPlayer :: Player
+    , _aiAi          :: AI
+    }
+makeLenses ''AIGameState
+
+type AIGame = StateT AIGameState IO
+
+randomAi :: GameState -> IO Move
+randomAi gs = do
+    let validMoves = moves gs
+    m <- randomRIO (0,length validMoves - 1)
+    return $ validMoves !! m
+
+runAiGame :: IO ()
+runAiGame = void $ execStateT aiGame $ AIGameState undefined undefined randomAi
+
+initializeGameState :: IO GameState
+initializeGameState = do
+    size <- putStr "What size board (3-8)? " >> getLine
+    let parsed= do
+            i <- readMay size
+            if i >= 3 && i <= 8 then Just i else Nothing
+    case parsed of
+        Nothing -> putStrLn "- Please input a number 3-8" >> initializeGameState
+        Just i -> return (initialGameState i)
+
+blackOrWhite :: IO Player
+blackOrWhite = do
+    p <- putStr "(w)hite or (b)lack? " >> getLine
+    if p == "w" then return Player1
+    else
+        if p == "b" then return Player2
+        else putStrLn "- Please input w or b" >> blackOrWhite
+
+getPlayersMove :: AIGame Move
+getPlayersMove = do
+    aiGs <- get
+    mS <- liftIO (putStr "Move? ") >> liftIO getLine
+    case parseString moveParser mempty mS of
+        Failure d -> liftIO (putDoc d >> putStrLn "") >> getPlayersMove
+        Success m -> if m `elem` moves (aiGs^.aiGameState)
+                     then return m
+                     else do
+                        _ <- liftIO $ putStrLn $ "Can't make this move: " ++ show m
+                        getPlayersMove
+
+aiGame :: AIGame ()
+aiGame = do
+    gs <- liftIO initializeGameState
+    p <- liftIO blackOrWhite
+    aiGameState .= gs
+    aiHumanPlayer .= p
+    let loop = do
+            aiLoop
+            s <- get
+            liftIO $ print (s^.aiGameState) >> putStrLn ""
+            unless (isJust $ s^.aiGameState^.gsGameOverState) loop
+    loop
+    s <- get
+    liftIO $ print $ s^.aiGameState^.gsGameOverState
+
+aiLoop :: AIGame ()
+aiLoop = do
+    s <- get
+    let isPlayersTurn = s^.aiGameState^.gsCurrPlayer == s^.aiHumanPlayer
+    m <- if isPlayersTurn
+         then getPlayersMove
+         else liftIO $ (s^.aiAi) (s^.aiGameState)
+    unless isPlayersTurn $ liftIO $ putStrLn $ "- AI: " ++ printMove (Right m)
+    aiGameState %= (`makeMove` m)
